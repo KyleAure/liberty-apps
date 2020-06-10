33ee4a22cf1b65b60de7add039f380e00617df5b
kyleaure <kylejaure@gmail.com>
2020-06-10 13:03:57 -0500

    update to use gradle

--- jdbc/src/test/java/it/websphereapps/jdbc/TestDatabase.java
+++ jdbc/src/test/java/it/websphereapps/jdbc/TestDatabase.java
@@ -8,7 +8,9 @@ import org.microshed.testing.testcontainers.ApplicationContainer;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.sql.DataSource;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 import java.time.Duration;
@@ -19,26 +21,26 @@ import static org.junit.jupiter.api.Assertions.fail;
 @MicroShedTest
 public class TestDatabase implements SharedContainerConfiguration {
 
-    @Container
-    public static OracleContainer oracle = new OracleContainer("wnameless/oracle-xe-11g-r2");
+    // @Container
+    // public static OracleContainer oracle = new OracleContainer("wnameless/oracle-xe-11g-r2");
 
-    @Container
-    public static ApplicationContainer app = new ApplicationContainer()
-            .withAppContextRoot("/jdbcoracle")
-            .withReadinessPath("/jdbcoracle/app/resource")
-            .withStartupTimeout(Duration.ofMinutes(1));
+    // @Container
+    // public static ApplicationContainer app = new ApplicationContainer()
+    //         .withAppContextRoot("/jdbcoracle")
+    //         .withReadinessPath("/jdbcoracle/app/resource")
+    //         .withStartupTimeout(Duration.ofMinutes(1));
     @Inject
-    private static TestResource dbo;
+    TestResource dbo;
 
-    @Override
-    public void startContainers() {
-        oracle.start();
-        app.withEnv("JDBC_DRIVER", "ojdbc8_g.jar")
-                .withEnv("JDBC_URL", oracle.getJdbcUrl())
-                .withEnv("USER", oracle.getUsername())
-                .withEnv("PASSWORD", oracle.getPassword());
-        app.start();
-    }
+    // @Override
+    // public void startContainers() {
+    //     oracle.start();
+    //     app.withEnv("JDBC_DRIVER", "ojdbc8_g.jar")
+    //             .withEnv("JDBC_URL", oracle.getJdbcUrl())
+    //             .withEnv("USER", oracle.getUsername())
+    //             .withEnv("PASSWORD", oracle.getPassword());
+    //     app.start();
+    // }
 
     @Test
     public void testPost() {
